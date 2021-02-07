225896c081f0b9fea24f07f62ebb2ad2c4f837b0
Anthony Ettinger <anthony@chovy.com>
2021-02-07 14:50:36 -0800

    shorts

--- shorts.sh
+++ shorts.sh
@@ -3,13 +3,17 @@
 xidel -s https://www.marketbeat.com/short-interest/ --xml --xquery '
 for $row in //tbody/tr
 	let $ticker := $row//td[1]//div[@class="ticker-area"]/normalize-space(text())
+	let $name := $row//td[1]//div[@class="title-area"]/normalize-space(text())
 	let $shares := $row//td[2]/normalize-space(text())
 	let $vol := $row//td[3]/normalize-space(text())
 	let $days := $row//td[6]/normalize-space(text())
 	let $float := $row//td[7]/normalize-space(text())
 	let $percent := $row//td[8]/normalize-space(text())
+	order by $float
+	where boolean($ticker)
 	return
 <ticker symbol="{$ticker}">
+	<name>{$name}</name>
 	<shares>{$shares}</shares>
 	<volume>{$vol}</volume>
 	<days>{$days}</days>
